89c766c5533340668975777732b4e448d6f7cbd0
Churchill <reghoyah@gmail.com>
2025-07-13 12:50:23 +0000

    calculate financial expenses

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = input('Enter your monthly income: ')
+monthly_expenses = input('Enter yot total monthly expenses: ')
+
+monthly_savings = int(monthly_income) - int(monthly_expenses)
+print(f"your monthly savings are {monthly_savings}")
+interest = 0.05
+
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest)
+print(f"Projected savings after one year is {projected_savings}")
