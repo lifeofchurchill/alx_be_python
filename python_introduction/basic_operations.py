1cd07502d5ae99360c4f386ba45aa6570150afbe
Churchill <reghoyah@gmail.com>
2025-07-13 08:24:28 +0000

    basic arithmetic functions

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+#addition operation
+number1 = int(10)
+number2 = int(25)
+
+additionsum = number1 + number2
+print(f"addition of {number1} and {number2} is {additionsum}")
+
+#subtraction operation
+subtractionsum = number1 - number2
+print(f"subtraction of {number1} and {number2} is {subtractionsum}")
+
+#multiplication operation 
+multiplysum = number1 * number2 
+print(f"multiplication of {number1} and {number2} is {multiplysum}")
