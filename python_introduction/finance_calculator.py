2baf0586a756dcc930e1b32727c620ad2bd159f6
Churchill <reghoyah@gmail.com>
2025-07-13 12:53:46 +0000

    updates on the strings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,8 +2,8 @@ monthly_income = input('Enter your monthly income: ')
 monthly_expenses = input('Enter yot total monthly expenses: ')
 
 monthly_savings = int(monthly_income) - int(monthly_expenses)
-print(f"your monthly savings are {monthly_savings}")
+print(f"your monthly savings are ${monthly_savings}")
 interest = 0.05
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * interest)
-print(f"Projected savings after one year is {projected_savings}")
+print(f"Projected savings after one year, with interest is: ${projected_savings}")
